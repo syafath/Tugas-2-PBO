0fcb6c49cceb5b6e0e276ad7d2936e74a5e1b583
SYAFA <83085638+syafath@users.noreply.github.com>
2022-12-17 23:49:29 +0700

    Delete Struk.java

--- Struk.java
+++
@@ -1,7 +0,0 @@
-public class Struk extends Petugas {
-    @Override
-    void cetakStruk() {
-        System.out.println("Id Tiket : ");
-        System.out.println("Total Pembayaran : ");
-    }
-}
